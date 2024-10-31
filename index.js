7d82e8c31aa228ff121362d8c1a32c94f521d780
Lazoraa <150687128+Lazoraa@users.noreply.github.com>
2024-10-31 20:37:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/+625723827283"
+}
+
+function redirInstagram() {
+    window.location.href = "https://instagram.com/Wldnabdll"
+}
+
+function openFigma1() {
+    window.location.href = "https://www.figma.com/file/6ThRpG0DdW5pr1CVUfpvop/Untitled?type=design&node-id=0-1&mode=design&t=PoUCTIPXecKM0R6I-0"
+}
+function openFigma2() {
+    window.location.href = "https://www.figma.com/file/nPRXY8LbW3l5g9W571ezik/Bakso?type=design&mode=design&t=PoUCTIPXecKM0R6I-0"
+}
+function openFigma3() {
+    window.location.href = "https://www.figma.com/file/nPRXY8LbW3l5g9W571ezik/Bakso?type=design&mode=design&t=PoUCTIPXecKM0R6I-0"
+}
+function openFigma4() {
+    window.location.href = "https://www.figma.com/file/ETy6CXZPjK1qkNBvXZyn3Q/Furniture?type=design&mode=design&t=PoUCTIPXecKM0R6I-0"
+}
